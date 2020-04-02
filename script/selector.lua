9a43f734b812789417730c5c4686f32928fbda3c
Richard Clamp <richardc@unixbeard.net>
2020-04-02 06:07:42 +0100

    Ask the road_network what that entity does
    This does seem to give useful information, we can see that the
    id is 1 in a small map.
    
    It occurs to me though that a simpler thing is out there.  Just a
    mouseover and tooltip.  New branch coming up.

--- script/selector.lua
+++ script/selector.lua
@@ -1,14 +1,18 @@
+local road_network = require("script/road_network")
+
 local on_player_selected_area = function(event)
     if event.item ~= 'transport-drones-selector-tool' then return end
 
     game.print("[TD] event "..serpent.line(event))
-
-    game.print("[TD] area "..serpent.line(event.area))
-    for _, entity in pairs(event.tiles) do
-        game.print("[TD] tiles "..serpent.line(entity))
-    end
-    for _, entity in pairs(event.entities) do
-        game.print("[TD] entity "..serpent.line(entity))
+    for _, thing in pairs(event.tiles, event.entities) do
+        local node = road_network.get_node(thing.surface.index, thing.position.x, thing.position.y)
+        local network = node and node["id"]
+        game.print("[TD] "..serpent.block({
+            surface = thing.surface.index,
+            x = thing.position.x,
+            y = thing.position.y,
+            network = network,
+        }))
     end
 end
 
